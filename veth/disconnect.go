aa36ec131b822942bc93fef981363b89f0a7ff40
Alonza0314 <alonza.tu@gmail.com>
2026-04-16 07:32:10 +0000

    fix: lint

--- veth/disconnect.go
+++ veth/disconnect.go
@@ -15,7 +15,7 @@ func Disconnect(ns1, ns2 string) error {
 	}
 	defer func() {
 		if err := netns.Set(originNs); err != nil {
-			fmt.Printf("failed to re-set to origin ns: %v", err)
+			fmt.Printf("failed to re-set to origin ns: %v\n", err)
 		}
 		if err := originNs.Close(); err != nil {
 			fmt.Printf("failed to close origin ns: %v\n", err)
@@ -34,17 +34,17 @@ func Disconnect(ns1, ns2 string) error {
 	defer ns2CloseFunc()
 
 	if err := netns.Set(ns1Fd); err != nil {
-		return fmt.Errorf("failed to set to namespace %s: %v\n", ns1, err)
+		return fmt.Errorf("failed to set to namespace %s: %v", ns1, err)
 	}
 
 	vethName := GetVethName(ns1, ns2)
 	link, err := netlink.LinkByName(vethName)
 	if err != nil {
-		return fmt.Errorf("failed to get link %s: %v\n", vethName, err)
+		return fmt.Errorf("failed to get link %s: %v", vethName, err)
 	}
 
 	if err := netlink.LinkDel(link); err != nil {
-		return fmt.Errorf("failed to delete link %s: %v\n", vethName, err)
+		return fmt.Errorf("failed to delete link %s: %v", vethName, err)
 	}
 
 	return nil
